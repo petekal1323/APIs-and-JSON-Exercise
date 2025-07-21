91cd9a1e73bec11b165ba7668aadbad21a35714f
petekal1323 <petekal1323@gmail.com>
2025-07-21 18:28:17 -0400

    quote generator

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using System.Net.Http;
+using System.Runtime.InteropServices.JavaScript;
+using Newtonsoft.Json.Linq;
+
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            QuoteGenerator.KanyeQuote();
+            QuoteGenerator.KanyeQuote();
         }
     }
 }
---
+++ APIsAndJSON/QuoteGenerator.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Net.Http;
+using System.Runtime.InteropServices.JavaScript;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON;
+
+public class QuoteGenerator
+{
+    public static void KanyeQuote()
+    {
+        var client = new HttpClient();
+            
+        var kanyeURL = "https://api.kanye.rest/";
+            
+        var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            
+        var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            
+        Console.WriteLine($"{kanyeQuote}");
+        Console.WriteLine($"Kanye: '{kanyeQuote}'");
+    }
+
+    public static void RonQuote()
+    {
+        var client = new HttpClient();
+            
+        var RonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            
+        var RonResponse = client.GetStringAsync(RonURL).Result;
+            
+        var ronQuote = JArray.Parse(RonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+    }
+}
